1b70f01bc5677bf8668c6fbc984a6b07ccde02ee
Bart Kooijmans <Bart.Kooijmans@teamrockstars.nl>
2021-07-07 13:22:35 +0200

    test with jasmine-data-provider-ts

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,8 +1,25 @@
 // Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
 
 import sum from './sum';
+import { TestSource } from "jasmine-data-provider-ts";
 
 test('adds 1 + 2 to equal 3 in TScript', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+describe("parameterised test using a function as source", function() {
+  function provider() {
+    return [
+      { a: 2, b: 3, expected: 5 },
+      { a: 14, b: 15, expected: 29 },
+    ];
+  }
+  
+  TestSource<any>(provider, function(data) {
+    it("should return value using operator +", function() {
+      const result = sum(data.a, data.b);
+      expect(result).toEqual(data.expected);
+    });
+  });
 });
 
